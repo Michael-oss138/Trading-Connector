76618723926cf1a745fb61fe2803fb4f0bb0dadd
Michael-oss138 <nwaizumichael0@gmail.com>
2025-11-27 12:11:51 +0100

    handled buy buy and sell signal extraction using parser

---
+++ parser.py
@@ -0,0 +1,20 @@
+import re
+
+
+def parse-signal(message: str):
+    pattern = r"(BUY|SELL)\s+([A-Z]{2,10})\s+([\d.]+)\s*@\s*([\d.]+)"
+    match = re.search(pattern, message.upper())
+
+    if match:
+        action = match.group(1)
+        coin = match.group(2)
+        amount = float(match(3))
+        price = flot(match(4))
+
+        return{
+            "action": action,
+            "coin": coin,
+            "amount": amount,
+            "price": price
+        }
+    return None
